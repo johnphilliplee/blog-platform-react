10174564569bf2df7be830e9e27537dd61d4d426
John Lee <johnlee.programming@gmail.com>
2024-06-17 10:41:11 +0800

    Add link from homepage to PostArticle

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Homepage() {
     return (
         <div>
         <h1>Homepage</h1>
         <p>This page will display a list of articles.</p>
+        <Link to="/post-article"> Post an Article </Link>        
         </div>
     );
 }
